10ebda29f7dd17d288954d62270bc8792291e2a6
a <a@tuxpa.in>
2023-09-01 21:34:34 -0500

    ok

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-
-'use strict'
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./uniswap-sdk-core.cjs.production.min.js')
-} else {
-  module.exports = require('./uniswap-sdk-core.cjs.development.js')
-}
